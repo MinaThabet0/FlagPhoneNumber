6c006d7cd69b90447b0a4271318427082ce58d2c
MinaHardTask <72362838+MinaThabet0@users.noreply.github.com>
2025-05-18 22:28:57 +0300

    update package

--- Package.swift
+++ Package.swift
@@ -6,20 +6,33 @@ import PackageDescription
 let package = Package(
     name: "FlagPhoneNumber",
     platforms: [
-        .iOS(.v11),
+        .iOS(.v9),
     ],
     products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "FlagPhoneNumber",
-            targets: ["FlagPhoneNumber"]),
+            targets: ["FlagPhoneNumber", "libPhoneNumber"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/iziz/libPhoneNumber-iOS.git", from: "0.9.15"),
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "libPhoneNumber",
+            dependencies: [],
+            path: "Sources/libPhoneNumber",
+            publicHeadersPath: ".",
+            cSettings: [
+                .headerSearchPath("Internal")
+            ]
+        ),
         .target(
             name: "FlagPhoneNumber",
-            dependencies: [.product(name: "libPhoneNumber_iOS", package: "libPhoneNumber-iOS")],
+            dependencies: ["libPhoneNumber"],
             path: "Sources",
             exclude: ["Info.plist", "libPhoneNumber"],
             publicHeadersPath: "."
